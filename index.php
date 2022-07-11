50ea102b6de45debe7c6749cb0f43c466032e0fe
AKGV1 <97513152+AKGV1@users.noreply.github.com>
2022-07-11 01:42:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title> AKG_xD </title>
+<link rel="stylesheet" href="index.css">
+<head>
+<body>
+<div id="loading">
+
+</div>
+<div id="headerbar">
+<img src="" width="50px" height="50px" >
+<H1>AKG_xD CybeR SHooTER<h1>
+
+</div>
+<div>
+<ul>
+<li> <a href="index.html"> HOME </li>
+<li> <a href="index.html"> RESOURCES</li>
+<li> <a href="index.html"> LACTURE's</li>
+<li> <a href="index.html"> CONTACT</li>
+</div>
+</body>
+</html>
